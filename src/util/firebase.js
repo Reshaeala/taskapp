513775c0bbb1c2cb07176c656882bbed96f59e97
Reshae Alagbada <reshaealagbada@Reshaes-MacBook-Pro.local>
2020-10-01 17:52:23 -0500

    checking to see if this will fix it

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,13 +1,8 @@
 import firebase from "firebase";
-<<<<<<< HEAD
-
-   var firebaseConfig = {
-=======
 import "firebase/auth";
 import "firebase/firestore";
 
   var firebaseConfig = {
->>>>>>> 7b2abb5fb7259c79088f3e4812c5e765df637253
     apiKey: "AIzaSyAp4Ce9RZ9wv0gKBItcbkriA3TvZjdJGtY",
     authDomain: "tasklist-290920.firebaseapp.com",
     databaseURL: "https://tasklist-290920.firebaseio.com",
@@ -17,16 +12,9 @@ import "firebase/firestore";
     appId: "1:362411408273:web:c17917d712a8369f7e3830",
     measurementId: "G-KX9XYY1V5X"
   };
-<<<<<<< HEAD
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  export default firebase;
-=======
 
  const fire = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
   export default fire;
->>>>>>> 7b2abb5fb7259c79088f3e4812c5e765df637253
